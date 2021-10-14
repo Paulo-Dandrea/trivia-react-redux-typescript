a0432db0d1d629ccca95b1f9edce7db045b20748
Paulo-Dandrea <prog.dandrea@gmail.com>
2021-10-14 09:22:54 -0300

    test: adjust cypress localStorage key for player

--- cypress/integration/project.spec.js
+++ cypress/integration/project.spec.js
@@ -8,7 +8,7 @@ const QUESTION_CATEGORY_SELECTOR = '[data-testid="question-category"]';
 const QUESTION_TEXT_SELECTOR = '[data-testid="question-text"]';
 const CORRECT_ALTERNATIVE_SELECTOR = '[data-testid="correct-answer"]';
 const WRONG_ALTERNATIVES_SELECTOR = '[data-testid*="wrong-answer"]';
-const LOCAL_STORAGE_STATE_KEY = 'state';
+const LOCAL_STORAGE_STATE_KEY = 'player';
 const BUTTON_NEXT_QUESTION_SELECTOR = '[data-testid="btn-next"]';
 const FEEDBACK_TEXT_SELECTOR = '[data-testid="feedback-text"]';
 
